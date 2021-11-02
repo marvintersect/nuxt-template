e37de85b8f782480e2ac31a5cada50de1f3a1a44
marvintersect <marvin@vintersect.com>
2021-11-02 11:34:11 +0100

    Logo change and Message edit.

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div class='mt-20'>
       <Logo style='width:120px;margin:auto' />
       <h1 class='text-center text-3xl mt-10'>
-        NuxtJS KickStart Template
+        Welcome to Vintersect LTD
       </h1>
       <h2 class='text-center'>
-        Create simple, beautiful, and elegant website.
+        It's all about Value for our Clients and their Business!
       </h2>
       
     </div>
